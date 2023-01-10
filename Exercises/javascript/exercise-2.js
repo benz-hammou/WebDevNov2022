27bdfcaf44ce063cb1db4db4e7d28bbebda8afe9
benz-hammou <Mohamed.Benhammo@student.intecbrussel.be>
2023-01-10 16:09:13 +0100

    update

--- Exercises/javascript/exercise-2.js
+++ Exercises/javascript/exercise-2.js
@@ -16,14 +16,22 @@ function multiply (first, second){
     }
     console.log(multiply(1, 2));
 // 4. Create a function called "divide" that takes two arguments and returns their quotient.
-
+function divide (first, second){
+    return first / second
+    }
+    console.log(divide(1, 2));
 
 // 5. Create a function called "calculate" that takes three arguments. Assume the two arguments are a number ie num1, num2 and a function called operates (a callback).
-
-
+function calculate (num1, operates, num2){
+    return eval(`${num1} ${operates} ${num2}`)
+    } 
 // 6. Call the calculate function 4 times, each time using one of the operation functions you wrote
-
-
+console.log(calculate(11, '*', 5))
+console.log(calculate(10, '+', 5))
+console.log(calculate(31, '-', 5))
+console.log(calculate(85, '/', 5))
 // 7. Create a function called "printDay" that takes one argument (a number from 1-7) and returns the day of the week (1 is Sunday, 2 is Monday etc.)
+function printDay (){
 
+}
 
