5fc7b820d4d69252cd2d2296c59d30e86e0e66eb
benz-hammou <Mohamed.Benhammo@student.intecbrussel.be>
2023-01-19 15:37:08 +0100

    .map new exe

--- Basics/javascript/ArrayMethods/index.js
+++ Basics/javascript/ArrayMethods/index.js
@@ -14,7 +14,7 @@ students.push(true) // boelean
 students.push(20) // Number
 students.push({
     id: 1,
-    name:"jimmy"
+    name: "jimmy"
 })
 
 // pop() => remove an item from array list 
@@ -35,8 +35,8 @@ delete students[9]
 result = students
 
 // concat()
-var n1=[1,2,3]
-var n2=[4,5,6]
+var n1 = [1, 2, 3]
+var n2 = [4, 5, 6]
 console.log(n1.concat(n2));
 
 // splice()
@@ -47,7 +47,7 @@ students.push('John Doe')
 result = students
 
 // slice()
-let newArrayList = students.slice(0,3)
+let newArrayList = students.slice(0, 3)
 result = newArrayList
 
 // reverse()
@@ -55,16 +55,65 @@ result = newArrayList.reverse()
 
 // sort()
 result = students.sort()
-var numbers = [4,450,0,36,6,7,98,563]
-result = numbers.sort(function(x,y){
-    return x-y
+var numbers = [4, 450, 0, 36, 6, 7, 98, 563]
+result = numbers.sort(function (x, y) {
+    return x - y
 })
 
 // forEach method
-students.forEach(function(value, index){
+/* students.forEach(function(value, index){
     console.log(`ID: ${index}\nValue: ${value}`);
 });
+console.log(`Result is\n${result}\ntypeof data is ${typeof result}`); */
 
+// map()
+students = ['Ahmad', 'Mehmed', 'Ali', 'Mohammed', 'Arhisa']
+/* result = students.map(function(name){
+    return name
+})
+console.log(result); */
 
-console.log(`Result is\n${result}\ntypeof data is ${typeof result}`);
+// filter()
+let scores = [33,44,55,66,88,99]
 
+/* result= scores.filter(function(score){
+    return score>44
+}) 
+ */
+const allStudents = [
+    {
+        name: "John",
+        score: 44
+    },
+    {
+        name: "Jimmy",
+        score: 55
+    },
+    {
+        name: "Michael",
+        score: 66
+    },
+    {
+        name: "Megan",
+        score: 77
+    },
+]
+
+/* result = allStudents.filter(function (student) {
+    return student.score > 44
+})
+ */
+
+//reduce
+/* let resultsum = scores.reduce(function(acc, val){
+    console.log(acc,"accu", val,"value")
+    return acc+=val
+}) */
+
+// includes()
+console.log(scores.includes(44));
+
+//score
+console.log(scores.indexOf(44)); 
+
+//console.log(result);
