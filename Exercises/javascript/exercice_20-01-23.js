72aa3138b0831ef379e3e129f13d2c53e0342cd9
benz-hammou <Mohamed.Benhammo@student.intecbrussel.be>
2023-01-23 11:39:42 +0100

    election

--- Exercises/javascript/exercice_20-01-23.js
+++ Exercises/javascript/exercice_20-01-23.js
@@ -1,4 +1,4 @@
-const students = [
+/* const students = [
   {
     _id: "63c993feb6f5dc691c2c0ac3",
     name: "Fannie Buckley",
@@ -44,14 +44,104 @@ const students = [
     name: "Velazquez Hensley",
     scores: [67, 45, 52],
   },
-];
+]; */
 // 1. Calculate the average score of sum of all scores for each student [reduce]
 // Hint [forEach(),reduce()]
+// let result = []
+// students.forEach(obj => {
+//   const scores = obj.scores
+//   const avarageOfEachStudent = scores.reduce((acc, val) => acc + val) / scores.length
+//   result.push(avarageOfEachStudent)
+// });
 
-const avarageScores = students.forEach(obj => {
+/* function getAvg(arr) {
+  let result = []
+  arr.forEach(obj => {
     const scores = obj.scores
-    const avarageOfEachStudent = scores.reduce((acc, val) => acc + val)/scores.length
-    return console.log(avarageOfEachStudent); 
-});
+    const avarageOfEachStudent = scores.reduce((acc, val) => acc + val) / scores.length
+    result.push(avarageOfEachStudent)
+  });
+  return result
+}
+console.log(getAvg(students)) */
 
-// why the result is not printing when i'm console outside ???  => console.log(avarageScores);
+const election = [
+  {
+      person_name:"John",
+      politic_party1:true,
+      politic_party2:false,
+      politic_party3:false
+  },
+  {
+      person_name:"Michael",
+      politic_party1:false,
+      politic_party2:false,
+      politic_party3:true
+  },
+  {
+      person_name:"Jimmy",
+      politic_party1:false,
+      politic_party2:true,
+      politic_party3:false
+  },
+  {
+      person_name:"Michel",
+      politic_party1:true,
+      politic_party2:false,
+      politic_party3:false
+  },
+  {
+      person_name:"Gabriel",
+      politic_party1:false,
+      politic_party2:false,
+      politic_party3:true
+  },
+  {
+      person_name:"Alberto",
+      politic_party1:false,
+      politic_party2:false,
+      politic_party3:true
+  }
+]
+
+election.forEach(function(item) {
+  /* console.log(item.politic_party1,"politic_party1");
+  console.log(item.politic_party2,"politic_party2");
+  console.log(item.politic_party3,"politic_party3"); */
+  let {politic_party1, politic_party2, politic_party3, person_name} = item
+  if(politic_party1){
+    console.log(`${person_name} voted for politic party 1`);
+    return politic_party1++
+  }
+  if(politic_party2){
+    console.log(`${person_name} voted for politic party 2`);
+    return politic_party2++
+  }
+  if(politic_party3){
+    console.log(`${person_name} voted for politic party 3 `);
+    return politic_party3++
+  }
+  console.log(Math.max(politic_party1, politic_party3));
+})
+
+/* function electionA(arr) {
+  let result = []
+  let party1 = 0
+  let party2 = 0
+  let party3 = 0
+  arr.forEach(obj => {
+    console.log(obj.politic_party1);
+    console.log(obj.politic_party2);
+    console.log(obj.politic_party3);
+    if(obj.politic_party1){
+      party1++
+    } else (obj.politic_party2){
+      party2++
+    }
+  });
+  console.log(party1)
+  console.log(party2)
+}
+//console.log(getAvg(students))
+
+electionA(election) */
