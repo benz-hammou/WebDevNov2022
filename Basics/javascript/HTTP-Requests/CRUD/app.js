28c1ed7a4859c83e5086d080d6123dd2e84c0768
benz-hammou <Mohamed.Benhammo@student.intecbrussel.be>
2023-03-12 21:09:40 +0100

    comment crud delete

--- Basics/javascript/HTTP-Requests/CRUD/app.js
+++ Basics/javascript/HTTP-Requests/CRUD/app.js
@@ -1,30 +1,48 @@
-const InputUsername = document.getElementById('username')
-const TextAreaComment = document.getElementById('comment')
-const CommentsList = document.getElementById('commentsList')
+const InputUsername = document.getElementById("username");
+const TextAreaComment = document.getElementById("comment");
+const CommentsList = document.getElementById("commentsList");
+const Data_ID = document.getElementById('data_id');
 
 function AddComment() {
-    const obj = {
-        username: InputUsername.value,
-        comment: TextAreaComment.value
-    }
-    fetch('http://localhost:3000/database',{
-        method:'POST',
-        headers:{
-            'Content-Type':'application/json'
-        },
-        body:JSON.stringify(obj)
+  const obj = {
+    username: InputUsername.value,
+    comment: TextAreaComment.value,
+  };
+  console.log(obj);
+  fetch("http://localhost:3000/database", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(obj),
+  })
+    .then((res) => res.json())
+
+    .then((data) => {
+      console.log(data, "saved");
+    });
+}
+
+// Recieves all comments (GET request)
+fetch("http://localhost:3000/database")
+.then((res) => res.json())
+.then((allcomments) => {
+    allcomments.map((single_comment) => {
+        console.log(single_comment); // single object from the array
+        CommentsList.innerHTML += `<div class="border rounded mb-2 d-flex justify-content-between" id="data_id">
+        <li class="list-group-item border-0"><strong>${single_comment.username}:</strong> ${single_comment.comment}</li>
+        <button type="button" class="btn btn-outline-danger border-0" onclick="RemoveComment()">X</button>
+        </div>`;
+    });
+    console.log([allcomments]);
+});
+
+function RemoveComment() {
+    const ID = Data_ID.value
+    fetch(`http://localhost:3000/database/${ID}`,{
+        method:'DELETE'
     })
-    .then(res=>res.json())
-    .then(data=>{
-        console.log(data,'saved');
+    .then(()=>{
+        console.log('data is deleted')
     })
 }
-// Recieves all comments (GET request)
-fetch('http://localhost:3000/database')
-.then(res=>res.json())
-.then(allcomments=>{
-    allcomments.map(single_comment=>{
-        console.log(single_comment) // single object from the array
-        CommentsList.innerHTML += `<li class="list-group-item"><strong>${single_comment.username}:</strong> ${single_comment.comment}</li>`
-    })
-})
